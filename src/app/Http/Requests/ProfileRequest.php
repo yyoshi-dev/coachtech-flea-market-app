1384aaab2ff50a54070826d3672cacfda2021710
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-02-01 20:50:38 +0900

    ProfileRequestの作成

--- src/app/Http/Requests/ProfileRequest.php
+++ src/app/Http/Requests/ProfileRequest.php
@@ -11,7 +11,7 @@ class ProfileRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,22 @@ class ProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'profile_image' => ['nullable', 'image', 'mimes:jpeg,png'],
+            'name' => ['required', 'max:20'],
+            'postal_code' => ['required', 'regex:/^[0-9]{3}-[0-9]{4}$/'],
+            'address' => ['required']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'profile_image.mimes' => 'プロフィール画像の拡張子は.jpegもしくは.pngでアップロードしてください',
+            'name.required' => 'お名前を入力してください',
+            'name.max' => 'お名前は20文字以下で入力してください',
+            'postal_code.required' => '郵便番号を入力してください',
+            'postal_code.regex' => '郵便番号はハイフンありの8文字で入力してください',
+            'address.required' => '住所を入力してください',
         ];
     }
 }
