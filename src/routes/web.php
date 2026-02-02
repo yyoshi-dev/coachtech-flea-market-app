da75fb64e5566fe33f174c817599d4376236afed
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-02-02 22:52:19 +0900

    CheckProfileCompletedミドルウェアを考慮してルーティングを修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,8 +21,14 @@ Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
 
-// 商品購入
+// プロフィール設定 (初回ログイン時)
 Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/mypage/profile', [ProfileController::class, 'showProfileEditPage']);
+    Route::post('/mypage/profile', [ProfileController::class, 'editProfile']);
+});
+
+// 商品購入
+Route::middleware(['auth', 'verified', 'profile.completed'])->group(function () {
     Route::get('/purchase/{item_id}', [PurchaseController::class, 'showPurchasePage']);
     Route::get('/purchase/address/{item_id}', [PurchaseController::class, 'showAddressEditPage']);
     Route::post('/purchase/payment/{item_id}', [PurchaseController::class, 'storePaymentMethodSelection']);
@@ -30,10 +36,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/purchase/address/{item_id}', [PurchaseController::class, 'updateAddress']);
     Route::get('/purchase/success/{item_id}', [PurchaseController::class, 'handleStripeSuccess']);
 
-
     Route::get('/mypage', [ProfileController::class, 'showProfilePage']);
-    Route::get('/mypage/profile', [ProfileController::class, 'showProfileEditPage']);
-    Route::post('/mypage/profile', [ProfileController::class, 'editProfile']);
     // Route::get('/sell', ...);
 });
 
