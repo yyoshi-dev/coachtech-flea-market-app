916118660077c71c5cbfce60d0bda25ed1d1a53e
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-02-09 23:21:30 +0900

    会員登録画面、ログイン画面、メール認証誘導画面のCSS作成

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -6,36 +6,36 @@
 
 @section('content')
 <div class="auth-content">
-    <div class="login-form">
-        <h2 class="login-form__heading content__heading">ログイン</h2>
+    <div class="auth-form">
+        <h2 class="auth-form__heading">ログイン</h2>
 
-        <div class="login-form__inner">
-            <form action="/login" method="post" class="login-form__form" novalidate>
+        <div class="auth-form__inner">
+            <form action="/login" method="post" class="auth-form__form" novalidate>
                 @csrf
 
-                <div class="login-form__group">
-                    <label for="email" class="login-form__label">メールアドレス</label>
-                    <input type="email" name="email" id="email" value="{{ old('email') }}" class="login-form__input">
+                <div class="auth-form__group">
+                    <label for="email" class="auth-form__label">メールアドレス</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" class="auth-form__input">
                     @error('email')
-                    <p class="login-form__error-message">{{ $message }}</p>
+                    <p class="auth-form__error-message">{{ $message }}</p>
                     @enderror
                 </div>
 
-                <div class="login-form__group">
-                    <label for="password" class="login-form__label">パスワード</label>
-                    <input type="password" name="password" id="password" class="login-form__input">
+                <div class="auth-form__group">
+                    <label for="password" class="auth-form__label">パスワード</label>
+                    <input type="password" name="password" id="password" class="auth-form__input">
                     @error('password')
-                    <p class="login-form__error-message">{{ $message }}</p>
+                    <p class="auth-form__error-message">{{ $message }}</p>
                     @enderror
                 </div>
 
                 <div>
-                    <input type="submit" value="ログインする" class="login-form__btn btn">
+                    <input type="submit" value="ログインする" class="auth-form__btn">
                 </div>
             </form>
         </div>
     </div>
 
-    <a href="/register" class="login-form__register-link">会員登録はこちら</a>
+    <a href="/register" class="auth-form__register-link">会員登録はこちら</a>
 </div>
 @endsection
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -6,52 +6,52 @@
 
 @section('content')
 <div class="auth-content">
-    <div class="register-form">
-        <h2 class="register-form__heading content__heading">会員登録</h2>
+    <div class="auth-form">
+        <h2 class="auth-form__heading">会員登録</h2>
 
-        <div class="register-form__inner">
-            <form action="/register" method="post" class="register-form__form" novalidate>
+        <div class="auth-form__inner">
+            <form action="/register" method="post" class="auth-form__form" novalidate>
                 @csrf
 
-                <div class="register-form__group">
-                    <label for="name" class="register-form__label">ユーザー名</label>
-                    <input type="text" name="name" id="name" value="{{ old('name') }}" class="register-form__input">
+                <div class="auth-form__group">
+                    <label for="name" class="auth-form__label">ユーザー名</label>
+                    <input type="text" name="name" id="name" value="{{ old('name') }}" class="auth-form__input">
                     @error('name')
-                    <p class="register-form__error-message">{{ $message }}</p>
+                    <p class="auth-form__error-message">{{ $message }}</p>
                     @enderror
                 </div>
 
-                <div class="register-form__group">
-                    <label for="email" class="register-form__label">メールアドレス</label>
-                    <input type="email" name="email" id="email" value="{{ old('email') }}" class="register-form__input">
+                <div class="auth-form__group">
+                    <label for="email" class="auth-form__label">メールアドレス</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" class="auth-form__input">
                     @error('email')
-                    <p class="register-form__error-message">{{ $message }}</p>
+                    <p class="auth-form__error-message">{{ $message }}</p>
                     @enderror
                 </div>
 
-                <div class="register-form__group">
-                    <label for="password" class="register-form__label">パスワード</label>
-                    <input type="password" name="password" id="password" class="register-form__input">
+                <div class="auth-form__group">
+                    <label for="password" class="auth-form__label">パスワード</label>
+                    <input type="password" name="password" id="password" class="auth-form__input">
                     @error('password')
-                    <p class="register-form__error-message">{{ $message }}</p>
+                    <p class="auth-form__error-message">{{ $message }}</p>
                     @enderror
                 </div>
 
-                <div class="register-form__group">
-                    <label for="password_confirmation" class="register-form__label">確認用パスワード</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation" class="register-form__input">
+                <div class="auth-form__group">
+                    <label for="password_confirmation" class="auth-form__label">確認用パスワード</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="auth-form__input">
                     @error('password_confirmation')
-                    <p class="register-form__error-message">{{ $message }}</p>
+                    <p class="auth-form__error-message">{{ $message }}</p>
                     @enderror
                 </div>
 
                 <div>
-                    <input type="submit" value="登録する" class="register-form__btn btn">
+                    <input type="submit" value="登録する" class="auth-form__btn">
                 </div>
             </form>
         </div>
     </div>
 
-    <a href="/login" class="register-form__login-link">ログインはこちら</a>
+    <a href="/login" class="auth-form__login-link">ログインはこちら</a>
 </div>
 @endsection
