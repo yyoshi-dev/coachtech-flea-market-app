df594f85c367962b0dbf1f9528b9384c1b24ceac
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-02-14 17:14:49 +0900

    不要な記述を削除

--- src/tests/Feature/Auth/LoginTest.php
+++ src/tests/Feature/Auth/LoginTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Auth;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
