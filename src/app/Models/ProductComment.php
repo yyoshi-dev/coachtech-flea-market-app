123d34c920d67ea0dd3d91db52be94eb884470c8
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2026-02-18 23:23:35 +0900

    ProductCommentFactoryの作成

--- src/app/Models/ProductComment.php
+++ src/app/Models/ProductComment.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductComment extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'product_id',
---
+++ src/database/factories/ProductCommentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\ProductComment;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductComment>
+ */
+class ProductCommentFactory extends Factory
+{
+    protected $model = ProductComment::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'product_id' => Product::factory(),
+            'comment' => fake()->text(200),
+        ];
+    }
+}
